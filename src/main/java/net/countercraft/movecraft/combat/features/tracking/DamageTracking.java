14560efa2bee90f8d74acb859cd42bfc2fcfb837
mohibazim26-cmd <mohibazim26@gmail.com>
2026-05-30 16:11:54 +0200

    Update DamageTracking.java

--- src/main/java/net/countercraft/movecraft/combat/features/tracking/DamageTracking.java
+++ src/main/java/net/countercraft/movecraft/combat/features/tracking/DamageTracking.java
@@ -112,40 +112,5 @@ public class DamageTracking implements Listener {
             damageRecords.put(craft, records);
         }
     }
-@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onPlayerVersusPlayerDamage(@NotNull org.bukkit.event.entity.EntityDamageByEntityEvent e) {
-        // Controlliamo se chi riceve il danno è un giocatore
-        if (!(e.getEntity() instanceof Player))
-            return;
 
-        Player vittima = (Player) e.getEntity();
-        Player attaccante = null;
-
-        // Identifichiamo l'attaccante (colpo diretto)
-        if (e.getDamager() instanceof Player) {
-            attaccante = (Player) e.getDamager();
-        } 
-        // Oppure proiettile (freccia, cannone, ecc.)
-        else if (e.getDamager() instanceof org.bukkit.entity.Projectile) {
-            org.bukkit.entity.Projectile proiettile = (org.bukkit.entity.Projectile) e.getDamager();
-            if (proiettile.getShooter() instanceof Player) {
-                attaccante = (Player) proiettile.getShooter();
-            }
-        }
-
-        // Se abbiamo un attaccante valido, registriamo il danno nel sistema Movecraft
-        if (attaccante != null) {
-            // Recuperiamo la nave della vittima estraendola direttamente dalla mappa dei piloti di Movecraft
-            var craftVittima = net.countercraft.movecraft.craft.CraftManager.getInstance().getPilotCraftMap().get(vittima);
-            
-            // Se la vittima sta effettivamente pilotando una nave, attiviamo il tracciamento
-            if (craftVittima instanceof PlayerCraft) {
-                Torpedo type = new Torpedo(); 
-                DamageRecord damageRecord = new DamageRecord(attaccante, vittima, type);
-                
-                // Lanciamo l'evento ufficiale: questo farà scattare in automatico CombatRelease!
-                Bukkit.getPluginManager().callEvent(new CraftDamagedByEvent((PlayerCraft) craftVittima, damageRecord));
-            }
-        }
-    }
 }
